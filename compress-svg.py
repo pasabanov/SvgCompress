5cbe65d70a9c1ff8087deb88cb2c90d37d501526
pasabanov <pasabanov@murena.io>
2024-08-17 23:56:30 +0300

    svgz support, refactoring

--- compress-svg.py
+++ compress-svg.py
@@ -20,71 +20,89 @@ import subprocess
 
 args = None
 svgo_path = None
+gzip_path = None
 
 
 def parse_arguments():
-	global args, svgo_path
+	global args, svgo_path, gzip_path
 	parser = argparse.ArgumentParser(description="Compress SVG files by removing unnecessary whitespace and comments.")
 	parser.add_argument('paths', nargs='+', help='List of directories or SVG files to compress.')
 	parser.add_argument('-v', '--version', action='version', version='SVG Compressor 1.0', help='Show the version of the script.')
 	parser.add_argument('-r', '--recursive', action='store_true', help='Recursively process directories.')
 	parser.add_argument('-f', '--remove-fill', action='store_true', help='Remove fill="..." attributes.')
 	parser.add_argument('--svgo', action='store_true', help='Use svgo if it exists in the system.')
+	parser.add_argument('--svgz', action='store_true', help='Compress to .svgz format with gzip utility after processing.')
 	args = parser.parse_args()
 	if args.svgo:
 		svgo_path = shutil.which('svgo')
+	if args.svgz:
+		gzip_path = shutil.which('gzip')
 
 
-RE_FILL = re.compile(r'fill="[^"]*"')
-RE_XLINK_HREF = re.compile(r'xlink:href')
-RE_XMLNS_XLINK = re.compile(r'\s+xmlns:xlink="[^"]*"')
-RE_COMMENT = re.compile(r'<!--.*?-->', flags=re.DOTALL)
-RE_XML_TAG = re.compile(r'<\?xml.*?>', flags=re.DOTALL)
-RE_DOCTYPE_SVG = re.compile(r'<!DOCTYPE svg[^>]*>')
-RE_WHITESPACE = re.compile(r'\s+')
-RE_WHITESPACE_AROUND_TAGS = re.compile(r'\s*(<|>)\s*')
-RE_SYMBOLS_BETWEEN_TAGS = re.compile(r'>[^<]+<')
-RE_XML_SPACE = re.compile(r'\s+xml:space="[^"]+"')
+def simple_compress(filepath):
 
+	# Define regular expressions once and store them as attributes of the function
+	if not hasattr(simple_compress, "RE_FILL"):
+		simple_compress.RE_FILL = re.compile(r'fill="[^"]*"')
+		simple_compress.RE_XLINK_HREF = re.compile(r'xlink:href')
+		simple_compress.RE_XMLNS_XLINK = re.compile(r'\s+xmlns:xlink="[^"]*"')
+		simple_compress.RE_COMMENT = re.compile(r'<!--.*?-->', flags=re.DOTALL)
+		simple_compress.RE_XML_TAG = re.compile(r'<\?xml.*?>', flags=re.DOTALL)
+		simple_compress.RE_DOCTYPE_SVG = re.compile(r'<!DOCTYPE svg[^>]*>')
+		simple_compress.RE_WHITESPACE = re.compile(r'\s+')
+		simple_compress.RE_WHITESPACE_AROUND_TAGS = re.compile(r'\s*(<|>)\s*')
+		simple_compress.RE_SYMBOLS_BETWEEN_TAGS = re.compile(r'>[^<]+<')
+		simple_compress.RE_XML_SPACE = re.compile(r'\s+xml:space="[^"]+"')
 
-def compress_svg_content(content):
+	with open(filepath, 'r', encoding='utf-8') as file:
+		content = file.read()
+
 	# Deleting whitespace at the ends
 	content = content.strip()
 	if args.remove_fill:
-		content = RE_FILL.sub('', content)
+		content = simple_compress.RE_FILL.sub('', content)
 	# If there is no xlink use, delete redundant attribute
-	if RE_XLINK_HREF.search(content) is None:
-		content = RE_XMLNS_XLINK.sub('', content)
+	if simple_compress.RE_XLINK_HREF.search(content) is None:
+		content = simple_compress.RE_XMLNS_XLINK.sub('', content)
 	# Deleting comments
-	content = RE_COMMENT.sub('', content)
+	content = simple_compress.RE_COMMENT.sub('', content)
 	# Deleting "<?xml" tag
-	content = RE_XML_TAG.sub('', content)
+	content = simple_compress.RE_XML_TAG.sub('', content)
 	# Deleting "<!DOCTYPE svg" tag
-	content = RE_DOCTYPE_SVG.sub('', content)
+	content = simple_compress.RE_DOCTYPE_SVG.sub('', content)
 	# Replacing whitespace with single space
-	content = RE_WHITESPACE.sub(' ', content)
+	content = simple_compress.RE_WHITESPACE.sub(' ', content)
 	# Removing spaces between angle brackets
-	content = RE_WHITESPACE_AROUND_TAGS.sub(r'\1', content)
+	content = simple_compress.RE_WHITESPACE_AROUND_TAGS.sub(r'\1', content)
 	# If there are no other symbols between angle brackets, delete redundant attribute
-	if RE_SYMBOLS_BETWEEN_TAGS.search(content) is None:
-		content = RE_XML_SPACE.sub('', content)
-	return content
+	if simple_compress.RE_SYMBOLS_BETWEEN_TAGS.search(content) is None:
+		content = simple_compress.RE_XML_SPACE.sub('', content)
 
-
-def process_file(filepath):
-	with open(filepath, 'r', encoding='utf-8') as file:
-		content = file.read()
-	compressed_content = compress_svg_content(content)
 	with open(filepath, 'w', encoding='utf-8') as file:
-		file.write(compressed_content)
+		file.write(content)
 
 
-def process_directory(directory):
+def compress_to_svgz(filepath):
+	if not args.svgz or gzip_path is None:
+		return
+	if not filepath.endswith('.svg'):
+		return
+	svgz_filepath = f"{filepath}z"
+	try:
+		with open(filepath, 'rb') as f_in:
+			with open(svgz_filepath, 'wb') as f_out:
+				subprocess.run([gzip_path, '-c', '-9'], stdin=f_in, stdout=f_out, check=True)
+		os.remove(filepath)
+	except subprocess.CalledProcessError as e:
+		print(f"Error compressing {filepath} to .svgz: {e}")
+
+
+def traverse_directory(directory, target_function):
 	for root, _, files in os.walk(directory):
 		for filename in files:
 			if filename.endswith('.svg'):
 				filepath = os.path.join(root, filename)
-				process_file(filepath)
+				target_function(filepath)
 		if not args.recursive:
 			break
 
@@ -99,9 +117,9 @@ def process_path(path):
 		return
 
 	if is_directory:
-		process_directory(path)
+		traverse_directory(path, simple_compress)
 	else:
-		process_file(path)
+		simple_compress(path)
 
 	if args.svgo and svgo_path is not None:
 		svgo_arguments = [svgo_path, '-q', path]
@@ -113,10 +131,18 @@ def process_path(path):
 		except subprocess.CalledProcessError as e:
 			print(f'Error during SVGO optimization for {path}: {e}')
 
+	if args.svgz and gzip_path is not None:
+		if is_directory:
+			traverse_directory(path, compress_to_svgz)
+		else:
+			compress_to_svgz(path)
+
 
 def main():
 	if args.svgo and svgo_path is None:
 		print("Error: svgo executable not found in the system.")
+	if args.svgz and gzip_path is None:
+		print("Error: gzip executable not found in the system.")
 	for path in args.paths:
 		process_path(path)
 
